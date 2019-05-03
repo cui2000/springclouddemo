2b1fbbf3c4e9f38fb8f6ead57eb6d143aefc4410
cui_2000 <cui_2000@163.com>
2019-05-03 18:07:57 +0800

    change properties

--- turbine/src/main/java/org/turbine/ServiceTurbineApplication.java
+++ turbine/src/main/java/org/turbine/ServiceTurbineApplication.java
@@ -3,12 +3,14 @@ package org.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 @EnableTurbine
 public class ServiceTurbineApplication {
 	public static void main(String[] args) {
